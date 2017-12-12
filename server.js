17ba067fb200bfc97b1a8d34fef5e58b0a5512a6
satenndrra <satenndrra@gmail.com>
2017-12-12 14:34:51 +0000

    added mongo container

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const mongodb = require('mongodb');
 
 // const PORT = process.env.PORT;
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 const app = express();
+const uri = "mongodb://mongo/node-docker-app";
 
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+    mongodb.MongoClient.connect(uri, function(err, db){
+        if (err) return console.log("error connecting with mongodb: " + err); 
+        db.collection.find().toArray(function(err,result){
+            if(err) return console.log("error reading mongodb data: " + err); 
+            res.send(result);
+        });
+    });    
 });
 
 app.listen(PORT, HOST);
