6c8fb1ac8fead41a0b0163950e4755c33ae946d7
satenndrra <satenndrra@gmail.com>
2017-12-12 08:37:11 +0000

    added port console statement

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+// const PORT = process.env.PORT;
 const PORT = 8080;
-const HOST = '0.0.0.0';
 
 const app = express();
+
+app.listen(process.env.PORT, function(){
+   console.log("listening to port 8080"); 
+});
 
 app.get('/', (req,res) => {
    res.send('Hello World'); 
 });
 
-console.log("App Started");
